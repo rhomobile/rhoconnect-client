a62d5ea3b821460233bcf0aec16a34a25d662fc5
Alex Epifanoff <alex.epifanoff@gmail.com>
2012-12-21 18:59:00 +0400

    updated API

--- ext/rhoconnect-client/ext/rhoconnect-client/shared/src/initRhoconnectClient.cpp
+++ ext/rhoconnect-client/ext/rhoconnect-client/shared/src/initRhoconnectClient.cpp
@@ -18,6 +18,10 @@ public:
 	virtual void clientRegisterCreate( const char* szDevicePin ) {
 		rho::sync::CClientRegister::Create(szDevicePin);
 	}
+
+	virtual void clientRegisterDestroy() {
+		rho::sync::CClientRegister::Destroy();
+	}
 	
 	virtual void rho_sync_addobjectnotify_bysrcname(const char* szSrcName, const char* szObject) {
 		rho_sync_addobjectnotify_bysrcname(szSrcName, szObject);
@@ -43,6 +47,10 @@ public:
 		rho::sync::CSyncThread::Create();
 	}
 	
+	virtual void syncThreadDestroy() {
+		rho::sync::CSyncThread::Destroy();
+	}
+	
 	virtual bool syncEngineNotifyIsReportingEnabled() {
 		return rho::sync::CSyncThread::getInstance()->getSyncEngine().getNotify().isReportingEnabled();
 	}
@@ -50,6 +58,79 @@ public:
 	virtual int rho_sync_issyncing() {
 		return rho_sync_issyncing();
 	}
+	
+	virtual int logged_in() {
+		return rho_sync_logged_in();
+	}
+	
+	virtual unsigned long login(const char* login, const char* password, const char* callback) {
+		return rho_sync_login(login, password, callback);
+	}
+	
+	virtual void logout() {
+		rho_sync_logout();
+	}
+	
+	virtual void stop() {
+		rho_sync_stop();
+	}
+	
+	virtual int set_pollinterval( int interval ) {
+		return rho_sync_set_pollinterval(interval);
+	}
+	
+	virtual int get_pollinterval() {
+		return rho_sync_get_pollinterval();
+	}
+	
+	virtual void set_syncserver( const char* syncserver ) {
+		rho_sync_set_syncserver(syncserver);
+	}
+	
+	virtual int get_pagesize() {
+		return rho_sync_get_pagesize();
+	}
+	
+	virtual void set_pagesize(int nPageSize) {
+		rho_sync_set_pagesize(nPageSize);
+	}
+	
+	virtual int get_lastsync_objectcount(int nSrcID) {
+		return rho_sync_get_lastsync_objectcount(nSrcID);
+	}
+	
+	virtual int issyncing() {
+		return rho_sync_issyncing();
+	}
+	
+	virtual void enable_status_popup(int b) {
+		rho_sync_enable_status_popup(b);
+	}
+	
+	virtual void set_threaded_mode(int b) {
+		rho_sync_set_threaded_mode(b);
+	}
+	
+	virtual void register_push() {
+		rho_sync_register_push();
+	}
+	
+	virtual void set_ssl_verify_peer(int b) {
+		rho_sync_set_ssl_verify_peer(b);
+	}
+	
+	virtual void setobjectnotify_url(const char* szUrl) {
+		rho_sync_setobjectnotify_url(szUrl);
+	}
+	
+	virtual void cleanobjectnotify() {
+		rho_sync_cleanobjectnotify();
+	}
+	
+	virtual void clear_notification(int srcID) {
+		rho_sync_clear_notification(srcID);
+	}
+
 
 };
 
