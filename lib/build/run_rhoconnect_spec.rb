87c6d1034cb0f628e39679d276ea46ebb5bfb9d4
Alexander Babichev <alexb@rhomobile.com>
2013-08-09 12:40:31 -0700

    Pull test_results.xml file from device to project dir after specs are over

--- lib/build/run_rhoconnect_spec.rb
+++ lib/build/run_rhoconnect_spec.rb
@@ -4,102 +4,110 @@ require File.join($rho_root,'lib','build','jake.rb')
 require File.join($rho_root,'lib','build','rhoconnect_helper.rb')
 
 
-	def run_rhoconnect_spec(platform,appname,flags)
-		test_appname = "testapp"
+def run_rhoconnect_spec(platform,appname,flags)
+	test_appname = "testapp"
 
-		puts "run_spec_app(#{platform},#{appname})"
-
-
-		rhobuildyml = File.join($rho_root,'rhobuild.yml')
-		$app_path = File.expand_path(File.join(File.dirname(__FILE__),'..','..','spec',appname))
-		puts "app path: #{$app_path}"
-
-		$app_config = Jake.config(File.open(File.join($app_path, "build.yml")))
-		config = Jake.config(File.open(rhobuildyml,'r'))
-
-		source_path = File.expand_path(File.join($app_path,'..','server'))
-		$tmp_path = File.join(File.dirname(__FILE__),'..','..','tmp')
-		
-		cleanup_apps
-		
-		FileUtils.mkdir_p File.expand_path($tmp_path)
-		server_path = File.expand_path(File.join($tmp_path,'testapp'))
-
-		$rhoconnect_bin = "#{$rhoconnect_root}/bin/rhoconnect"
-		puts "$rhoconnect_bin: #{$rhoconnect_bin}"
-
-		RhoconnectHelper.set_rhoconnect_bin "#{$rhoconnect_root}/bin/rhoconnect"
-		puts "rhoconnect_bin: #{RhoconnectHelper.rhoconnect_bin}"
-		
-		
-		RhoconnectHelper.set_rc_out File.open( File.join($app_path, "rhoconnect.log" ), "w")
-		RhoconnectHelper.set_redis_out File.open( File.join($app_path, "redis.log" ), "w")
-		RhoconnectHelper.set_resque_out File.open( File.join($app_path, "resque.log" ), "w")
-		RhoconnectHelper.set_rc_push_out File.open( File.join($app_path, "rc_push.log" ), "w")
-		
-		RhoconnectHelper.set_enable_push(false)
-		RhoconnectHelper.set_enable_rails(false)
-		RhoconnectHelper.set_enable_redis($rhoconnect_use_redis)
-		
-		RhoconnectHelper.stop_rhoconnect_stack
+	puts "run_spec_app(#{platform},#{appname})"
 
 
-		puts "generate app"
-		res = RhoconnectHelper.generate_app($tmp_path,test_appname)
+	rhobuildyml = File.join($rho_root,'rhobuild.yml')
+	$app_path = File.expand_path(File.join(File.dirname(__FILE__),'..','..','spec',appname))
+	puts "app path: #{$app_path}"
 
-		puts "patching Gemfile with correct rhoconnect path"
-		target_gemfile = File.join(server_path, 'Gemfile')
-		file = File.read(target_gemfile)
-		file.gsub!(/(gem 'rhoconnect'*.*)/, "gem 'rhoconnect', :path => '#{$rhoconnect_root}'")
-		File.open(target_gemfile, 'w') {|f| f.write file}
+	$app_config = Jake.config(File.open(File.join($app_path, "build.yml")))
+	config = Jake.config(File.open(rhobuildyml,'r'))
 
-		puts "patching Gemfile with aws-s3"
-		File.open(target_gemfile, 'a') {|f| f.puts "gem 'aws-s3', '>= 0.6.3'" }
+	source_path = File.expand_path(File.join($app_path,'..','server'))
+	$tmp_path = File.join(File.dirname(__FILE__),'..','..','tmp')
 
-		puts "patching Gemfile with sqlite3"
-		File.open(target_gemfile, 'a') {|f| f.puts "gem 'sqlite3', '>= 1.3.3'" }
+	cleanup_apps
 
-		puts "bundle install"
-		Kernel.system("bundle","install",:chdir => server_path)
+	FileUtils.mkdir_p File.expand_path($tmp_path)
+	server_path = File.expand_path(File.join($tmp_path,'testapp'))
 
-		puts "adding source files"
-		FileUtils.cp_r ["#{source_path}/sources","#{source_path}/settings"], server_path
+	$rhoconnect_bin = "#{$rhoconnect_root}/bin/rhoconnect"
+	puts "$rhoconnect_bin: #{$rhoconnect_bin}"
 
-		puts "cleanup rhoconnect data"
-		FileUtils.rm_r(File.join(server_path,"data")) if File.directory?(File.join(server_path,"data"))
+	RhoconnectHelper.set_rhoconnect_bin "#{$rhoconnect_root}/bin/rhoconnect"
+	puts "rhoconnect_bin: #{RhoconnectHelper.rhoconnect_bin}"
 
 
-		RhoconnectHelper.start_rhoconnect_stack(server_path,true)
-        
-        generateRuby = flags && flags[:ruby]==true
-        generateJS = flags && flags[:js]==true
-        
-        if generateRuby then
-            File.open(File.join($app_path, 'app', 'sync_server.rb'), 'w') do |f|
-                f.puts "SYNC_SERVER_HOST = '#{RhoconnectHelper.host}'"
-                f.puts "SYNC_SERVER_PORT = #{RhoconnectHelper.port}"
-            end
-        end
-        
-        if generateJS then
-            File.open(File.join($app_path, 'public', 'app', 'sync_server.js'), 'w') do |f|
-                f.puts "var SYNC_SERVER_HOST = '#{RhoconnectHelper.host}';"
-                f.puts "var SYNC_SERVER_PORT = #{RhoconnectHelper.port};"
-            end
-        end
+	RhoconnectHelper.set_rc_out File.open( File.join($app_path, "rhoconnect.log" ), "w")
+	RhoconnectHelper.set_redis_out File.open( File.join($app_path, "redis.log" ), "w")
+	RhoconnectHelper.set_resque_out File.open( File.join($app_path, "resque.log" ), "w")
+	RhoconnectHelper.set_rc_push_out File.open( File.join($app_path, "rc_push.log" ), "w")
 
-		puts "run specs"
-		chdir $rho_root
-		Rake::Task.tasks.each { |t| t.reenable }
-		run_specs = $device ? "run:#{platform}:#{$device}:spec" : "run:#{platform}:spec"
-    	Rake::Task[run_specs].invoke
+	RhoconnectHelper.set_enable_push(false)
+	RhoconnectHelper.set_enable_rails(false)
+	RhoconnectHelper.set_enable_redis($rhoconnect_use_redis)
 
-	ensure
-		RhoconnectHelper.stop_rhoconnect_stack
-		cleanup_apps
+	RhoconnectHelper.stop_rhoconnect_stack
 
-		puts "run_spec_app(#{platform},#{appname}) done"
+
+	puts "generate app"
+	res = RhoconnectHelper.generate_app($tmp_path,test_appname)
+
+	puts "patching Gemfile with correct rhoconnect path"
+	target_gemfile = File.join(server_path, 'Gemfile')
+	file = File.read(target_gemfile)
+	file.gsub!(/(gem 'rhoconnect'*.*)/, "gem 'rhoconnect', :path => '#{$rhoconnect_root}'")
+	File.open(target_gemfile, 'w') {|f| f.write file}
+
+	puts "patching Gemfile with aws-s3"
+	File.open(target_gemfile, 'a') {|f| f.puts "gem 'aws-s3', '>= 0.6.3'" }
+
+	puts "patching Gemfile with sqlite3"
+	File.open(target_gemfile, 'a') {|f| f.puts "gem 'sqlite3', '>= 1.3.3'" }
+
+	puts "bundle install"
+	Kernel.system("bundle","install",:chdir => server_path)
+
+	puts "adding source files"
+	FileUtils.cp_r ["#{source_path}/sources","#{source_path}/settings"], server_path
+
+	puts "cleanup rhoconnect data"
+	FileUtils.rm_r(File.join(server_path,"data")) if File.directory?(File.join(server_path,"data"))
+
+
+	RhoconnectHelper.start_rhoconnect_stack(server_path,true)
+
+  generateRuby = flags && flags[:ruby]==true
+  generateJS = flags && flags[:js]==true
+
+  if generateRuby then
+    File.open(File.join($app_path, 'app', 'sync_server.rb'), 'w') do |f|
+      f.puts "SYNC_SERVER_HOST = '#{RhoconnectHelper.host}'"
+      f.puts "SYNC_SERVER_PORT = #{RhoconnectHelper.port}"
+    end
+  end
+
+  if generateJS then
+    File.open(File.join($app_path, 'public', 'app', 'sync_server.js'), 'w') do |f|
+      f.puts "var SYNC_SERVER_HOST = '#{RhoconnectHelper.host}';"
+      f.puts "var SYNC_SERVER_PORT = #{RhoconnectHelper.port};"
+    end
+  end
+
+	puts "run specs"
+	chdir $rho_root
+	Rake::Task.tasks.each { |t| t.reenable }
+	run_specs = $device ? "run:#{platform}:#{$device}:spec" : "run:#{platform}:spec"
+	Rake::Task[run_specs].invoke
+
+	if platform == "android"
+		path_to_results = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+	  if system("adb -d pull /data/data/#{$app_package_name}/rhodata/apps/test_results.xml #{path_to_results}") # if $app_package_name
+	  	puts "Test results pulled from device to #{path_to_results}/test_results.xml file"
+	  end
 	end
+	Rake::Task["uninstall:#{platform}:device"].invoke
+
+ensure
+	RhoconnectHelper.stop_rhoconnect_stack
+	cleanup_apps
+	puts "run_spec_app(#{platform},#{appname}) done"
+
+end
 
 def cleanup_apps
 	puts "cleanup"
